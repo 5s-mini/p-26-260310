75bb225f02a9f86fd08b3297ab9ddc2bc7fe5c7e
5s-mini <dnclsehd123@naver.com>
2026-03-11 15:02:33 +0900

    fix: Valid 어노테이션 추가

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -4,13 +4,19 @@ import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import com.back.global.rsData.RsData;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.jmx.ParentAwareNamingStrategy;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -48,7 +54,7 @@ public class ApiV1PostController {
     ){}
 
     @PostMapping
-    public RsData<PostDto> write(@RequestBody postWriteReqBody reqBody) {
+    public RsData<PostDto> write(@RequestBody @Valid postWriteReqBody reqBody) {
         Post post = postService.write(reqBody.title, reqBody.content);
 
         return new RsData<>(
